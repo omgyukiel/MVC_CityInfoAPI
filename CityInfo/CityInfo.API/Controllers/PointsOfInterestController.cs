0215e9f13365188e5d5de76a26b0d2000e466ffc
KennyLevu <kennylevu1@gmail.com>
2025-08-30 21:20:37 -0700

    feat(api): add http patch poi

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,5 +1,6 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -87,6 +88,44 @@ namespace CityInfo.API.Controllers
             poi.Description = pointOfInterest.Description;
 
             return NoContent();
+        }
+        [HttpPatch("{pointofinterestid}")]
+        public ActionResult PartiallyUpdatePointOfInterest(
+            int cityId, int pointOfInterestId,
+            JsonPatchDocument<PointOfInterestForUpdateDto> patchDocument)
+        {
+            var city = CitiesDataStore.Instance.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var poi = city.PointsOfInterest.FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (poi == null)
+            {
+                return NotFound();
+            }
+            var poiPatch =
+                new PointOfInterestForUpdateDto()
+                {
+                    Name = poi.Name,
+                    Description = poi.Description
+                };
+            patchDocument.ApplyTo(poiPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!TryValidateModel(poiPatch))
+            {
+                return BadRequest(ModelState);
+            }
+            poi.Name = poiPatch.Name;
+            poi.Description = poiPatch.Description;
+
+            return NoContent();
+
         }
     }
 }
