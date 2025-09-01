6576e1c36ab1ef23d70600718993de3f48d79562
KennyLevu <kennylevu1@gmail.com>
2025-08-31 19:31:41 -0700

    fix(db): register correct connection string reference

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -42,8 +42,9 @@ builder.Services.AddTransient<IMailService, LocalMailService>();
 builder.Services.AddTransient<IMailService, CloudMailService>();
 #endif
 builder.Services.AddSingleton<CitiesDataStore>();
-builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite(
-    builder.Configuration["ConnectionStrings.CityInfoDBConnectionString"]));
+builder.Services.AddDbContext<CityInfoContext>(
+    dbContextOptions => dbContextOptions.UseSqlite(
+        builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
 
 var app = builder.Build();
