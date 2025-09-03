a504dcdb2acfc1bc39488ff8917fd4eb70a5199f
KennyLevu <kennylevu1@gmail.com>
2025-09-02 21:04:17 -0700

    docs(openapi): add versioned api spec to swagger

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,4 +1,5 @@
 using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
 using AutoMapper;
 using CityInfo.API;
 using CityInfo.API.DbContexts;
@@ -29,13 +30,9 @@ builder.Services.AddControllers(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(setupAction =>
-{
-    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
-    setupAction.IncludeXmlComments(xmlCommentsFullPath);
-});
+
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer(options =>
     {
@@ -65,7 +62,37 @@ builder.Services.AddApiVersioning(setupAction =>
     setupAction.ReportApiVersions = true;
     setupAction.AssumeDefaultVersionWhenUnspecified = true;
     setupAction.DefaultApiVersion = new ApiVersion(1, 0);
-}).AddMvc();
+}).AddMvc()
+.AddApiExplorer(setupAction =>
+{
+    setupAction.GroupNameFormat = "'v'VVV";
+    setupAction.SubstituteApiVersionInUrl = true;
+});
+
+var apiVersionDescriptionProvider = builder.Services.BuildServiceProvider()
+    .GetService<IApiVersionDescriptionProvider>();
+
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+    {
+        setupAction.SwaggerDoc(
+            $"{description.GroupName}",
+            new()
+            {
+                Title = $"City Info API {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "An API to manage cities and their points of interest.",
+                Contact = new()
+                {
+                    Name = "omgyukiel"
+                }
+            });
+    }
+        var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+});
 
 builder.Services.AddProblemDetails(options =>
 {
@@ -99,7 +126,16 @@ if (!app.Environment.IsDevelopment())
 else if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(setupAction =>
+    {
+        var description = app.DescribeApiVersions();
+        foreach (var item in description)
+        {
+            setupAction.SwaggerEndpoint(
+                $"/swagger/{item.GroupName}/swagger.json",
+                item.GroupName.ToUpperInvariant());
+        }
+    });
 }
 
 app.UseHttpsRedirection();
